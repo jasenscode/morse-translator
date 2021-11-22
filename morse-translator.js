4208a685b6f44ec93b93e8e7b04e5d86b233dfbc
Jasen <60624556+jasenscode@users.noreply.github.com>
2021-11-22 12:12:44 +0000

    Added test function for numbers to test file

--- morse-translator.js
+++ morse-translator.js
@@ -1,29 +1,7 @@
-import { it, expect, describe } from "@jest/globals";
-
-// test individual letters
-// it should translate a to .-
-// it should translate z to --..
-
-describe("translate single letters to morse", () => {
-  it("should translate a to .-", () => {
-    const result = translateLetter("a");
-    expect(result).toBe(".-");
-  });
-
-  it("should translate z to --..", () => {
-    const result = translateLetter("z");
-    expect(result).toBe("--..");
-  });
-});
-
-// test numbers
-// it should translate 2 to ..---
-// it should translate 56 to ..... -....
-
-// test complete words
-// it should translate sos to ... --- ...
-// it should translate hello to .... . .-.. .-.. ---
-
-// test punctuation
-// it should translate comma to --..--
-// it should translate apostrophe to .----.
+export const translateLetter = (letter) => {
+  if (letter === "a") {
+    return ".-";
+  } else if (letter === "z") {
+    return "--..";
+  }
+};
---
+++ morse-translator.test.js
@@ -0,0 +1,42 @@
+import { it, expect, describe } from "@jest/globals";
+import { translateLetter } from "./morse-translator.js";
+
+// test individual letters
+// it should translate a to .-
+// it should translate z to --..
+
+describe("translate single letters to morse", () => {
+  it("should translate a to .-", () => {
+    const result = translateLetter("a");
+    expect(result).toBe(".-");
+  });
+
+  it("should translate z to --..", () => {
+    const result = translateLetter("z");
+    expect(result).toBe("--..");
+  });
+});
+
+// test numbers
+// it should translate 2 to ..---
+// it should translate 56 to ..... -....
+
+describe("translate numbers to morse", () => {
+  it("should translate 2 to ..---", () => {
+    const result = translateNumber(2);
+    expect(result).toBe("..---");
+  });
+
+  it("should translate 56 to ..... -....", () => {
+    const result = translateNumber(56);
+    expect(result).toBe("..... -....");
+  });
+});
+
+// test complete words
+// it should translate sos to ... --- ...
+// it should translate hello to .... . .-.. .-.. ---
+
+// test punctuation
+// it should translate comma to --..--
+// it should translate apostrophe to .----.
