03196eba03b875ab615e690ac7d00bbf4b91315c
Jasen <60624556+jasenscode@users.noreply.github.com>
2021-11-22 12:07:05 +0000

    Wrote actual test conditions in test file

--- morse-translator.js
+++ morse-translator.js
@@ -1,6 +1,20 @@
+import { it, expect, describe } from "@jest/globals";
+
 // test individual letters
 // it should translate a to .-
 // it should translate z to --..
+
+describe("translate single letters to morse", () => {
+  it("should translate a to .-", () => {
+    const result = translateLetter("a");
+    expect(result).toBe(".-");
+  });
+
+  it("should translate z to --..", () => {
+    const result = translateLetter("z");
+    expect(result).toBe("--..");
+  });
+});
 
 // test numbers
 // it should translate 2 to ..---
