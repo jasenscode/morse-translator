9b0435abfbcf8cd3bf234f0113bb91a2bca44021
Jasen <60624556+jasenscode@users.noreply.github.com>
2021-11-22 15:10:13 +0000

    Created variables in js file, including inputting morse alphabet into an object

---
+++ main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//Selectors
+const userInput = document.querySelector(".user-input");
+const translationOutput = document.querySelector(".translate-output");
+const morseAlphabet = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+};
