9412e89c06cf3637ae1657d3d98d71f2d423c692
Jasen <60624556+jasenscode@users.noreply.github.com>
2021-11-22 12:06:02 +0000

    Added psuedo code to test file outlining test conditions

---
+++ morse-translator.js
@@ -0,0 +1,15 @@
+// test individual letters
+// it should translate a to .-
+// it should translate z to --..
+
+// test numbers
+// it should translate 2 to ..---
+// it should translate 56 to ..... -....
+
+// test complete words
+// it should translate sos to ... --- ...
+// it should translate hello to .... . .-.. .-.. ---
+
+// test punctuation
+// it should translate comma to --..--
+// it should translate apostrophe to .----.
